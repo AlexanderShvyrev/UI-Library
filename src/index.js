62fe1c778b5b7d92687aaea90a09d82ebda6511b
Alexander Shvyrev <shurik_stv@yahoo.com>
2020-11-10 11:57:50 -0500

    added tooltip and dropdown

--- src/index.js
+++ src/index.js
@@ -1,1 +1,15 @@
-console.log('hello ninjas')
+import Tooltip from './ninja-ui/tooltip'
+import Dropdown from './ninja-ui/dropdown'
+
+
+//create a tooltip
+const tooltip = new Tooltip(document.querySelector('.tooltip'))
+tooltip.init()
+
+//create dropdowns
+const dropdowns = document.querySelectorAll('.dropdown')
+
+dropdowns.forEach(dropdown => {
+    const instance = new Dropdown(dropdown)
+    instance.init()
+})
